82a1801f262f35670a62dff8ed056763f735a6a9
User <User@LAB2-14>
2024-09-24 13:03:10 +0300

    Add project files.

---
+++ AppServer/DatabaseCreation.sql
@@ -0,0 +1,31 @@
+﻿﻿Use master
+Go
+IF EXISTS (SELECT * FROM sys.databases WHERE name = N'AppServer_DB')
+BEGIN
+    DROP DATABASE AppServer_DB;
+END
+Go
+Create Database AppServer_DB
+Go
+Use AppServer_DB
+Go
+
+CREATE TABLE Users (
+    UserId INT PRIMARY KEY Identity,     --מפתח ראשי--
+    Mail NVARCHAR(100),      --מייל של משתמש--
+    Username NVARCHAR(100),  --שם משתמש--
+    [Password] NVARCHAR(100),    --סיסמה--
+    [Name] NVARCHAR(100),   --שם קונה/קונדיטוריה--
+    FOREIGN KEY (UserTypeId) REFERENCES UserTypes(UserTypeId),  --סוג משתמש--
+    [Image] VARBINARY(MAX)  --תמונת פרופיל--
+    );
+
+    CREATE TABLE BAKERS (
+    FOREIGN KEY (BakerId) REFERENCES Users(UserId),
+    HighestPrice FLOAT,
+    FOREIGN KEY (ConfectioneryTypeId) REFERENCES ConfectioneryTypes(ConectioneryTypeId)
+    FOREIGN KEY (StatusCode) REFERENCES Statuses(StatusCode),
+    Profits FLOAT
+    );
+
+    
